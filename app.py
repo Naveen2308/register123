db3ef1a8842f3972f2fb0b7e3a5bb3685716a333
Naveen2308 <89527717+Naveen2308@users.noreply.github.com>
2023-12-23 19:45:12 +0530

    app.py

--- app.py
+++ app.py
@@ -1,37 +1,42 @@
 from flask import Flask, request, jsonify
-from flask_pymongo import PyMongo
-from flask_cors import CORS
+from flask_cors import CORS  # Import CORS
+from pymongo import MongoClient
+import hashlib
 
 app = Flask(__name__)
-CORS(app)
-# Replace <YOUR_CONNECTION_STRING> with your actual MongoDB Atlas connection string
-app.config['MONGO_URI'] = "mongodb+srv://system:admin@cluster0.3kzqwqf.mongodb.net/registrationsdata?retryWrites=true&w=majority"
-mongo = PyMongo(app)
+CORS(app)  # Enable CORS for all routes
 
-# API endpoint for registration
+# Your MongoDB setup goes here
+mongo_uri = "mongodb+srv://system:admin@cluster0.3kzqwqf.mongodb.net/registrationsdata?retryWrites=true&w=majority"
+client = MongoClient(mongo_uri)
+db = client["registrationsdata"]
+users_collection = db["users"]
+
 @app.route('/register', methods=['POST'])
 def register():
-    data = request.form
+    try:
+        # Retrieve user data from the request
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
 
-    username = data.get('username')
-    email = data.get('email')
-    password = data.get('password')
+        # Hash the password using a secure method (e.g., bcrypt)
+        hashed_password = hashlib.sha256(password.encode()).hexdigest()
 
-    # Insert registration data into MongoDB
-    users_collection = mongo.db.users
-    user_id = users_collection.insert_one({
-        'username': username,
-        'email': email,
-        'password': password
-    }).inserted_id
+        # Check if the username or email already exists in the database
+        existing_user = users_collection.find_one({'$or': [{'username': username}, {'email': email}]})
+        if existing_user:
+            return jsonify({'error': 'Username or email already exists'}), 400
 
-    response = {
-        'status': 'success',
-        'message': 'Registration successful!',
-        'user_id': str(user_id)
-    }
+        # Insert the new user into the database
+        user_data = {'username': username, 'email': email, 'password': hashed_password}
+        result = users_collection.insert_one(user_data)
 
-    return jsonify(response)
+        # Return the user ID in a standardized response format
+        return jsonify({'data': {'user_id': str(result.inserted_id)}}), 200
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    except Exception as e:
+        print(f"Error during registration: {str(e)}")
+        return jsonify({'error': 'Internal server error'}), 500
+
+
